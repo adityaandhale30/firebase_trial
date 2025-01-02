cba00774c02dfe87f7430f57cf896c9960ecf65d
AdiByte30 <adityaandhale30@gmail.com>
2025-01-02 15:42:35 +0530

    feat - firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyA3_-y455AmYbSphiWZvuqvcFmK5BHeDmw",
+        appId: "1:769451998620:android:caecf9565dff35ea8b1c59",
+        messagingSenderId: "769451998620",
+        projectId: "llb3-5e525"),
+  );
   runApp(const MainApp());
-  
 }
 
 class MainApp extends StatelessWidget {
@@ -19,7 +27,12 @@ class MainApp extends StatelessWidget {
           child: Text('Hello World!'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () async {
+            log("dddddddd");
+            Map<String, dynamic> data = {"iq": 10, "age": 21};
+            await FirebaseFirestore.instance.collection("Gavhane").add(data);
+            log("adeddd");
+          },
         ),
       ),
     );
